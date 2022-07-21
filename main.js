cf80df9607ce968eebf6770fc3ff32143bd58fdc
Vit111 <busuevaulia692@gmail.com>
2022-07-21 20:09:10 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+
+// Zadacha N1
+// let values = function (...num){
+//     let x = [].concat(...num)
+//     console.log(x)
+// }
+
+// values([1, 2, 3],[1, 2, 3],[1, 2, 3],[1, 2, 3],[1, 2, 3],[1, 2, 3],[1, 2, 3])
+
+
+// Zadacha N2
+// let word = prompt('What is your word?', '').trim();
+// function palindrome(str) {
+//     if(str.split('').reverse().join('') == str){
+//         y = 'true'
+//     }
+//     else{
+//         y = 'false'
+//     }
+//     console.log(y)
+//   }
+//   palindrome(word)
+
+// Zadacha N3
+// let addNumber = function (...numbers) {
+//     let x = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         x += numbers[i];
+//     }
+//     return x;
+// };
+// console.log(addNumber(9, 3, 5, 6, 7, 87, 333333333333, 2, 1, 3));
+
+// let numbers = [9, 3, 5, 6, 7, 87, 333333333333, 2, 1, 3];
+// let x = 0;
+// numbers.forEach(function(el, index, array) {
+//     x += numbers[index];
+//     return x;
+// });
+// console.log(x)
+
+
+
+
+
+
+
+
+
+
+
